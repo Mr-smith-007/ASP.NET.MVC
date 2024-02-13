4fd9cc6875837facf08fe4a07a3b54183c419869
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-02-13 14:57:58 +0300

    Добавьте файлы проекта.

---
+++ ASP.NET.MVC/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+using System;
+
+namespace ASP.NET.MVC.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+
+            await _next.Invoke(context);
+        }        
+    }
+}
+
